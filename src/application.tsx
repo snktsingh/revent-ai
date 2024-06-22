bce84d640d3e450c9013fc11fb0e8c8ca0317b02
Shivakrishna Kosari <shivakrishna.kosari@revent.ai>
2024-06-22 14:11:22 +0530

    added feedback component

--- src/application.tsx
+++ src/application.tsx
@@ -1,10 +1,14 @@
 import AppRoutes from './router/route';
 import { Token, isAuth, tempToken } from './utils/localStorage/data';
 import DefaultRoutes from './router/unAuthRoute';
+import Feedback from './common-ui/feedback';
 
 const Application = () => {
   if (isAuth && tempToken) {
-    return <AppRoutes />;
+    return <>
+    <AppRoutes />
+    <Feedback />
+    </>;
   } else {
     return <DefaultRoutes />;
   }
